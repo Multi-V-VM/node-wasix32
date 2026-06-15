ffad8da126e47e9d9234d0694e88132d37fcc93e
vickieGPT <victoryang00@ucsc.edu>
2026-06-15 00:52:23 +0000

    wasm32: add compiler instruction scheduler stub

---
+++ deps/v8/src/compiler/backend/wasm32/instruction-scheduler-wasm32.cc
@@ -0,0 +1,41 @@
+// Copyright 2026 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if V8_TARGET_ARCH_WASM32
+
+#include "src/compiler/backend/instruction-codes.h"
+#include "src/compiler/backend/instruction-scheduler.h"
+#include "src/compiler/backend/instruction.h"
+
+namespace v8 {
+namespace internal {
+namespace compiler {
+
+bool InstructionScheduler::SchedulerSupported() { return false; }
+
+int InstructionScheduler::GetTargetInstructionFlags(
+    const Instruction* instr) const {
+  switch (instr->arch_opcode()) {
+    case kWasm32StoreSlot:
+    case kWasm32StoreMem8:
+    case kWasm32StoreMem16:
+    case kWasm32StoreMem32:
+    case kWasm32CallBuiltin:
+    case kWasm32CallRuntime:
+    case kWasm32Return:
+      return kHasSideEffect;
+    default:
+      return kNoOpcodeFlags;
+  }
+}
+
+int InstructionScheduler::GetInstructionLatency(const Instruction* instr) {
+  return 1;
+}
+
+}  // namespace compiler
+}  // namespace internal
+}  // namespace v8
+
+#endif  // V8_TARGET_ARCH_WASM32
